431be1301ecdbca3cc467cfc6a6634f061179c39
bryancury3r <gabriel_bryancury@outlook.com>
2022-08-03 22:08:03 -0300

    add function active and remove active

--- js/main.js
+++ js/main.js
@@ -1,5 +1,13 @@
 // Cart
 
-let cartIcon = document.querySelector('#cart-icon')
-let cart = document.querySelector('.cart')
-let closeCart = document.querySelector('#close-cart')
+let cartIcon = document.querySelector('#cart-icon');
+let cart = document.querySelector('.cart');
+let closeCart = document.querySelector('#close-cart');
+
+cartIcon.onclick = () => {
+  cart.classList.add("active");
+}
+
+closeCart.onclick = () => {
+  cart.classList.remove("active");
+}
