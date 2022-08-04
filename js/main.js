99d1581c2a875a7c262f688b5e68b7841823d4e6
bryancury3r <gabriel_bryancury@outlook.com>
2022-08-03 23:36:41 -0300

    add function removeItem

--- js/main.js
+++ js/main.js
@@ -4,10 +4,35 @@ let cartIcon = document.querySelector('#cart-icon');
 let cart = document.querySelector('.cart');
 let closeCart = document.querySelector('#close-cart');
 
+// Open Cart
 cartIcon.onclick = () => {
   cart.classList.add("active");
-}
+};
 
+// Close Cart
 closeCart.onclick = () => {
   cart.classList.remove("active");
+};
+
+// Cart Working JS
+if (document.readyState == 'loading') {
+  document.addEventListener('DOMContentLoaded', ready)
+}else {
+  ready();
+}
+
+// Making Function
+function ready() {
+  // Remove Itens From Cart
+  var reomveCartButtons = document.getElementsByClassName('cart-remove');
+  console.log(reomveCartButtons)
+  for (var i = 0; i < reomveCartButtons.length; i++) {
+    var button = reomveCartButtons[i]
+    button.addEventListener('click', removeCartItem)
+  }
+}
+
+function removeCartItem(event) {
+  var buttonClicked = event.target
+  buttonClicked.parentElement.remove()
 }
